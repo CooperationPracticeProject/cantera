7f0e700c2d9e153ee16bf7cbd764543b2c12b2c1
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2025-01-18 15:49:09 +0800

    feat: 新增邮件发送功能，包含简单文本和HTML邮件支持

---
+++ backend/src/main/java/com/bytedance/util/Email.java
@@ -0,0 +1,55 @@
+package com.bytedance.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.Resource;
+import jakarta.mail.internet.MimeMessage;
+import lombok.SneakyThrows;
+
+@Component
+public class Email {
+
+  @Resource
+  private JavaMailSender mailSender;
+
+  @Value("${spring.mail.username}")
+  private String from;
+
+  /**
+   * 发送简单文本邮件
+   * 
+   * @param to
+   * @param subject
+   * @param content
+   */
+  public void sendSimpleMail(String to, String subject, String content) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(from);
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(content);
+    mailSender.send(message);
+  }
+
+  /**
+   * 发送 Html 邮件
+   * 
+   * @param to
+   * @param subject
+   * @param content
+   */
+  @SneakyThrows
+  public void sendHtmlMail(String to, String subject, String content) {
+    MimeMessage message = mailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+    helper.setFrom(from);
+    helper.setTo(to);
+    helper.setSubject(subject);
+    helper.setText(content, true);
+    mailSender.send(message);
+  }
+}
