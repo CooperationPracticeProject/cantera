7b9dea5c5e7780b707c4f3c7f63252f5e441840a
lovecxx <1874488574@qq.com>
2025-02-12 14:43:25 +0800

    feat:添加OrderService内容

--- backend/src/main/java/com/bytedance/service/OrderService.java
+++ backend/src/main/java/com/bytedance/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.bytedance.service;
 
+import com.bytedance.model.dto.OrdersSubmitDTO;
 import com.bytedance.model.entity.Order;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.bytedance.model.vo.OrderSubmitVo;
 
 /**
  * @author darling
@@ -9,5 +11,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @createDate 2025-01-17 22:30:41
  */
 public interface OrderService extends IService<Order> {
-
+    OrderSubmitVo submitOrder(OrdersSubmitDTO ordersSubmitDTO);
 }
