e2ae351a7fd8045af775948b9bb953cd42e405ac
992527949 <992527949@qq.com>
2025-03-06 13:58:17 +0800

    feat: 重新实现getPermissionList和getRoleList方法以返回用户角色和权限

--- backend/src/main/java/com/bytedance/config/StpInterfaceImpl.java
+++ backend/src/main/java/com/bytedance/config/StpInterfaceImpl.java
@@ -37,21 +37,6 @@ public class StpInterfaceImpl implements StpInterface {
      */
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询权限
-        List<String> list = new ArrayList<String>();
-        // 把Object类型转换为long类型
-        long userId = Long.parseLong(loginId.toString());
-        long roleId = userMapper.selectById(userId).getRoleId();
-        // 根据角色id查询角色名称存入列表
-        list.add(roleMapper.selectById(roleId).getRoleName());
-        return list;
-    }
-
-    /**
-     * 返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
-     */
-    @Override
-    public List<String> getRoleList(Object loginId, String loginType) {
         // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询角色
         List<String> list = new ArrayList<String>();
         // 把Object类型转换为long类型
@@ -68,6 +53,21 @@ public class StpInterfaceImpl implements StpInterface {
                 list.add(permission.getPermName());
             }
         }
+        return list;
+    }
+
+    /**
+     * 返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
+     */
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询权限
+        List<String> list = new ArrayList<String>();
+        // 把Object类型转换为long类型
+        long userId = Long.parseLong(loginId.toString());
+        long roleId = userMapper.selectById(userId).getRoleId();
+        // 根据角色id查询角色名称存入列表
+        list.add(roleMapper.selectById(roleId).getRoleName());
         return list;
     }
 }
